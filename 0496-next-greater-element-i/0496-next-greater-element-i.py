122b3c00d8e255edc8adb7ed527d896fda37016a
Bhargavi Saraswat <saraswatbhargavi26@gmail.com>
2026-08-18 17:35:32 +0530

    Time: 3 ms (40.58%), Space: 19.4 MB (68.42%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        arr = [-1]*len(nums1)
+        freq = {}
+        c = 0
+        for i in range(len(nums2)):
+            freq[nums2[i]] = i
+        for i in range(len(nums1)):
+            j = freq[nums1[i]]+1
+            while j<len(nums2):
+                if nums1[i]<nums2[j]:
+                    arr[i] = nums2[j]
+                    break
+                j+=1
+        return arr
+
+
+
