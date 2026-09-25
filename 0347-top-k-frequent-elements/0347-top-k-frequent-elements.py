a3f1468a23d69c3e8cf0fce7080bd102ec5fc500
Bhargavi Saraswat <saraswatbhargavi26@gmail.com>
2026-09-25 12:57:22 +0530

    Time: 11 ms (24.68%), Space: 24.7 MB (20.91%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -8,7 +8,7 @@ class Solution:
             arr[val].append(key)
         res = []
         for i in range(len(arr)-1,0,-1):
-            for num in arr[i]:
+            for num in arr[i]: #avoids empty slots
                 res.append(num)
                 if len(res)==k:
                     return res
